daa3ae6dbb1e36ab89ade1a87f62098af9655dd1
Katerina Tuma <patch.and.hoodie@gmail.com>
2023-01-12 23:34:50 +0300

    popular__contant

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@
 window.addEventListener('load', windowLoad);
 
 function windowLoad() {
+
+   document.body.classList.add('loaded');
+
+
    if (document.querySelector('.main-slider')) {
       new Swiper('.main-slider', {
          speed: 2000,
@@ -17,4 +21,31 @@ function windowLoad() {
          },
       });
    }
-}
+
+   document.addEventListener("click", documentAction);
+
+   function documentAction(e) {
+      const targetEl = e.target;
+
+      if (targetEl.closest('.nav-popular__item')) {
+         const tabNavItem = targetEl.closest('.nav-popular__item');
+         if (!tabNavItem.classList.contains('active')) {
+            const activeTabNavItem = document.querySelector('.nav-popular__item.active');
+            activeTabNavItem.classList.remove('active');
+            tabNavItem.classList.add('active');
+
+            const tabItem = document.querySelectorAll('.popular__tab');
+            const activeTabItem = document.querySelector('.popular__tab.active');
+
+            activeTabItem.classList.remove('active');
+            tabItem[getIndex(tabNavItem)].classList.add('active');
+         }
+      }
+   }
+
+   function getIndex(elem) {
+      return Array.from(elem.parentNode.children).indexOf(elem);
+   }
+
+
+} 
